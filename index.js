14a4f8dfed2fac66d7067923738a93bbf7734191
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2023-12-20 23:32:55 +0600

    create get  post api for coffeshop and connected mongodb

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 3000;
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+//middelware
+app.use(cors());
+app.use(express.json());
+
+
+//mongoDB Connections
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jz0ivtr.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const coffeeCollection = client.db('coffeeDB').collection('coffee');
+    
+    app.get('/coffee', async(req,res) =>{
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
+    app.post('/coffee', async(req,res) =>{
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req,res)=>{
+    res.send('Coffe Store Server is Running!!!');
+})
+
+app.listen(port, ()=>{
+    console.log('Coffe Store is running on Port ',port);
+})
